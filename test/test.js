7c834c7c94be3540337a7428a70980490a987119
matthieufmt <matt136@hotmail.fr>
2023-02-14 14:44:00 +0100

    second commit

---
+++ test/test.js
@@ -0,0 +1,11 @@
+const assert = require("chai").assert;
+
+function add(x, y) {
+  return x + y;
+}
+
+describe("add", function () {
+  it("should add two numbers", function () {
+    assert.equal(add(1, 2), 3);
+  });
+});
