d322e4ced4df215d8d9369a144ce273c91027a72
matthieufmt <matt136@hotmail.fr>
2023-02-15 11:38:05 +0100

    eslint

--- test/test.js
+++ test/test.js
@@ -5,7 +5,7 @@ function add(x, y) {
 }
 
 describe("add", function () {
-  it("should add two numbers", function () {
+  it("should add two numbers.", function () {
     assert.equal(add(1, 2), 3);
   });
 });
